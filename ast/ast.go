571b0980d38408ae75c374b212be0708cddf9530
sondreal <sondre@villdyr.no>
2023-07-01 13:22:12 +0200

    added the interfaces to the AST

---
+++ ast/ast.go
@@ -0,0 +1,17 @@
+// ast/ast.go
+
+package ast
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
