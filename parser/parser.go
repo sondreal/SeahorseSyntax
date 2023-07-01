30200fefb4f993da346f9e3e7abd9c3bdbf8c4ea
sondreal <sondre@villdyr.no>
2023-07-01 13:51:17 +0200

    first impl. of the parser

---
+++ parser/parser.go
@@ -0,0 +1,34 @@
+// parser/parser.go
+
+package parser
+
+import (
+	"seahorsesyntax.villdyr.dev/ast"
+	"seahorsesyntax.villdyr.dev/lexer"
+	"seahorsesyntax.villdyr.dev/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
