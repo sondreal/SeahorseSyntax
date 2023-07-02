3cf0ec0393551b06eee85f329fc6e26ddb39ee41
sondreal <sondre@villdyr.no>
2023-07-02 11:37:18 +0200

    return statement structure

--- ast/ast.go
+++ ast/ast.go
@@ -46,3 +46,11 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
+
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
